be22cc5496174e8d2a62f1f3fcb10ae74536997f
Parsa Mh <157930237+parsamh8@users.noreply.github.com>
2024-11-28 19:41:12 -0500

    add fixture and e2e

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -2,4 +2,42 @@ describe('template spec', () => {
   it('connected to local host', () => {
     cy.visit('/')
   })
+
+
+ 
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
+  it('should get first questions', () => {
+    cy.intercept('GET', '/api/questions/random', {
+
+      fixture: "mockQuestion"
+      
+    }).as('getData');
+    cy.get('button').click()
+    cy.wait('@getData').then((intercept) => {
+      assert.isNotNull(intercept.response?.body, '1st API call has data');
+
+      const firstQ = intercept.response?.body[0]
+      expect(firstQ).to.have.property('question', 'Do you have any feelings?');
+    })
+  })
+
+  it('should get the second question', () => {
+    cy.intercept('GET', '/api/questions/random', {
+
+      fixture: "mockQuestion"
+      
+    }).as('getData');
+    cy.get('button').click()
+    cy.wait('@getData').then((intercept) => {
+      assert.isNotNull(intercept.response?.body, '1st API call has data');
+
+      const secondQ = intercept.response?.body[1]
+      expect(secondQ).to.have.property('question', 'Do you know Me?');
+    })
+  })
+
 })
+
